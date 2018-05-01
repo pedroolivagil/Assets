9d48e4f261d32caed0d90e4101908fd160db2959
pedroolivagil <onion_oliva@hotmail.com>
2018-05-01 21:13:58 +0200

    V.0.0.3

--- scripts/Scenes/MainMenuScene.cs
+++ scripts/Scenes/MainMenuScene.cs
@@ -3,11 +3,14 @@
 namespace Scenes{
     public class MainMenuScene : MonoBehaviour{
         private InstantGuiElement _dialog;
+        private InstantGuiElement _windowSettings;
 
         void Start(){
             _dialog = GameManager.GetGuiComponentWithName("dialogExit");
+            _windowSettings = GameManager.GetGuiComponentWithName("windowSettings");
         }
 
+// Dialogo de salida de juego
         void OptionButtonExit(){
             Debug.Log("Btn Exit has pressed");
             _dialog.gameObject.SetActive(true);
@@ -20,6 +23,22 @@ namespace Scenes{
 
         void DialogCancelExit(){
             _dialog.gameObject.SetActive(false);
+        }
+
+// Ventana de configuración 
+        void OptionButtonSettings(){
+            Debug.Log("Btn Settings has pressed");
+            _windowSettings.gameObject.SetActive(true);
+        }
+
+        void CancelWindowSettings(){
+            // Hacer rollback de las settings si es necesario y dejarlas como estaban antes
+            _windowSettings.gameObject.SetActive(false);
+        }
+
+        void AcceptWindowSettings(){
+            // aceptar las configuraciones del usuario
+            _windowSettings.gameObject.SetActive(false);
         }
     }
 }
