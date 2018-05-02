ea24d8168997f88793446a4e2b6079bf9fee9fdd
pedroolivagil <onion_oliva@hotmail.com>
2018-05-02 02:14:16 +0200

    V.0.0.4

--- scripts/Game/NetworkManager.cs
+++ scripts/Game/NetworkManager.cs
@@ -1,17 +1,24 @@
 ﻿using Photon;
 
-public class NetworkManager : MonoBehaviour{
-    public string Version;
+namespace Game{
+    public class NetworkManager : MonoBehaviour{
+        public string Version;
 
-    void Start(){
-        PhotonNetwork.ConnectUsingSettings(Version);
-    }
+        void Start(){
+            DontDestroyOnLoad(gameObject);
+            NewConnectionPhoton();
+        }
 
-    void OnConnectedToMaster(){
-        PhotonNetwork.JoinOrCreateRoom("Galaxy 1", new RoomOptions(){MaxPlayers = 10}, null);
-    }
+        void NewConnectionPhoton(){
+            PhotonNetwork.ConnectUsingSettings(Version);
+        }
 
-    void OnJoinedRoom(){
-        PhotonNetwork.Instantiate("Sphere", transform.position, transform.rotation, 0);
+        void OnConnectedToMaster(){
+            PhotonNetwork.JoinOrCreateRoom("Galaxy 1", new RoomOptions{MaxPlayers = 10}, null);
+        }
+
+        void OnJoinedRoom(){
+//        PhotonNetwork.Instantiate("Sphere", transform.position, transform.rotation, 0);
+        }
     }
 }
