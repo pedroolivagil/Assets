9946af6a92c28d060f9d774968b08f311fd09882
pedroolivagil <onion_oliva@hotmail.com>
2018-05-02 22:52:42 +0200

    V.0.0.5

--- scripts/Scenes/MainMenuScene.cs
+++ scripts/Scenes/MainMenuScene.cs
@@ -4,10 +4,12 @@ namespace Scenes{
     public class MainMenuScene : MonoBehaviour{
         private InstantGuiElement _dialog;
         private InstantGuiElement _windowSettings;
+        private InstantGuiElement _windowUser;
 
         void Start(){
             _dialog = GameManager.GetGuiComponentWithName("dialogExit");
             _windowSettings = GameManager.GetGuiComponentWithName("windowSettings");
+            _windowUser = GameManager.GetGuiComponentWithName("windowUser");
         }
 
 // Dialogo de salida de juego
@@ -39,6 +41,25 @@ namespace Scenes{
         void AcceptWindowSettings(){
             // aceptar las configuraciones del usuario
             _windowSettings.gameObject.SetActive(false);
+        }
+
+// Ventana de login
+        void OptionButtonPlay(){
+            Debug.Log("Btn Play has pressed");
+            _windowUser.gameObject.SetActive(true);
+        }
+
+        void SignUpWindowPlay(){
+            Debug.Log("Btn SignUp has pressed");
+            _windowUser.gameObject.SetActive(false);
+        }
+
+        void LoginWindowPlay(){
+            Debug.Log("Btn Login has pressed");
+            _windowUser.gameObject.SetActive(false);
+            InstantGuiElement userName = GameManager.GetGuiComponentWithName("userName");
+            InstantGuiElement passWord = GameManager.GetGuiComponentWithName("passWord");
+            Debug.Log("Data -> " + userName.text + " : " + passWord.text);            
         }
     }
 }
