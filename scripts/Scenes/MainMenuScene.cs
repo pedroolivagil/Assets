16183a28e84da080eed6b6acbe06e36fbea7ab36
pedroolivagil <onion_oliva@hotmail.com>
2018-05-03 20:10:32 +0200

    V.0.0.5

--- scripts/Scenes/MainMenuScene.cs
+++ scripts/Scenes/MainMenuScene.cs
@@ -13,53 +13,33 @@ namespace Scenes{
         }
 
 // Dialogo de salida de juego
-        void OptionButtonExit(){
-            Debug.Log("Btn Exit has pressed");
-            _dialog.gameObject.SetActive(true);
-        }
-
         void DialogExitGame(){
             // Hacemos un fadOut de la pantalla o un fadeIn Oscuro y pasamos a la pantalla de salida
             GameManager.ChangeScreen(GameScenes.ExitScreen);
         }
 
-        void DialogCancelExit(){
-            _dialog.gameObject.SetActive(false);
-        }
-
 // Ventana de configuración 
-        void OptionButtonSettings(){
-            Debug.Log("Btn Settings has pressed");
-            _windowSettings.gameObject.SetActive(true);
-        }
-
         void CancelWindowSettings(){
             // Hacer rollback de las settings si es necesario y dejarlas como estaban antes
-            _windowSettings.gameObject.SetActive(false);
+//            _windowSettings.gameObject.SetActive(false);
         }
 
         void AcceptWindowSettings(){
             // aceptar las configuraciones del usuario
-            _windowSettings.gameObject.SetActive(false);
+//            _windowSettings.gameObject.SetActive(false);
         }
 
 // Ventana de login
-        void OptionButtonPlay(){
-            Debug.Log("Btn Play has pressed");
-            _windowUser.gameObject.SetActive(true);
-        }
 
         void SignUpWindowPlay(){
             Debug.Log("Btn SignUp has pressed");
-            _windowUser.gameObject.SetActive(false);
         }
 
         void LoginWindowPlay(){
             Debug.Log("Btn Login has pressed");
-            _windowUser.gameObject.SetActive(false);
-            InstantGuiElement userName = GameManager.GetGuiComponentWithName("userName");
-            InstantGuiElement passWord = GameManager.GetGuiComponentWithName("passWord");
-            Debug.Log("Data -> " + userName.text + " : " + passWord.text);            
+            InstantGuiInputText userName = (InstantGuiInputText) GameManager.GetGuiComponentWithName("userName");
+            InstantGuiInputText passWord = (InstantGuiInputText) GameManager.GetGuiComponentWithName("passWord");
+            Debug.Log("Data -> " + userName.text + " : " + passWord.text);
         }
     }
 }
