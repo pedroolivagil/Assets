eac67d00b2fc226a57290426e29a1b29f15f9f1b
pedroolivagil <onion_oliva@hotmail.com>
2018-05-12 18:26:14 +0200

    V.0.1.12

---
+++ scripts/Game/Game/player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour{
+    public Camera Camera;
+    public float Speed = 3.0f;
+    public float Rotate = 100f;
+
+    private Vector3 offset; //Private variable to store the offset distance between the player and camera
+
+    void Start(){
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset = Camera.transform.position - transform.position;
+    }
+
+    void Update(){
+        Movement();
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate(){
+        UpdateCamera();
+    }
+
+    private void UpdateCamera(){
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        Camera.transform.position = transform.position + offset;
+    }
+
+    private void Movement(){
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * Speed;
+        var y = Time.deltaTime * Speed;
+        Vector3 movement = new Vector3(0, 0, x * -1);
+        transform.Rotate(movement, Time.deltaTime * Speed * Rotate);
+        transform.Translate(x, y, 0);
+    }
+}
