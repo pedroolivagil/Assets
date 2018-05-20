76c27e161191328099ae7f8f3655e389699398ed
pedroolivagil <onion_oliva@hotmail.com>
2018-05-21 01:44:26 +0200

    V.0.1.21

--- scripts/Game/Game/Enemy.cs
+++ scripts/Game/Game/Enemy.cs
@@ -9,7 +9,7 @@ namespace Game.Game{
         public float Speed = 3.0f;
         public float Rotate = 100f;
         public float MinDist = 10f;
-        public float MaxDist = 5f;
+        public float RadiusDetectPlayer = 5f;
         public float TimeShooting = 1;
         public bool PlayerDetected;
 
@@ -17,6 +17,7 @@ namespace Game.Game{
         private Transform _targetPlayer;
         private player _player;
         private Canon[] _canons;
+        private SphereCollider _sphereCollider;
         private float _dir = 0;
         private float _normalSpeed;
         private float _smoothTime = .3f;
@@ -26,6 +27,8 @@ namespace Game.Game{
             _normalSpeed = Speed;
             _healthBar = gameObject.GetComponentInChildren<Health>();
             _canons = gameObject.GetComponentsInChildren<Canon>();
+            _sphereCollider = gameObject.GetComponentInChildren<SphereCollider>();
+            _sphereCollider.radius = RadiusDetectPlayer;
             _healthBar.gameObject.SetActive(ShowHealthBar);
             StartCoroutine(UpdateDir());
         }
@@ -38,17 +41,15 @@ namespace Game.Game{
                 }
             }
             Movement();
-            Shoot();
+//            Shoot();
         }
 
         private IEnumerator UpdateDir(){
-            if (!PlayerDetected){
-                yield return new WaitForSeconds(1);
-                _dir = 0;
-                yield return new WaitForSeconds(GameManager.RandomBetween(5, 20));
-                _dir = GameManager.RandomBetween(-1, 1);
-                yield return UpdateDir();
-            }
+            yield return new WaitForSeconds(1);
+            _dir = 0;
+            yield return new WaitForSeconds(GameManager.RandomBetween(5, 20));
+            _dir = GameManager.RandomBetween(-1, 1);
+            yield return UpdateDir();
         }
 
         private void Movement(){
@@ -61,32 +62,41 @@ namespace Game.Game{
                     transform.Rotate(movement, Time.deltaTime * Speed * Rotate);
                     transform.Translate(x, y, 0);
                 } else{
-                    if (_targetPlayer != null){
-                        if (Vector3.Distance(transform.position, _targetPlayer.position) > MinDist){
-                            //move if distance from target is greater than 1
-                            Vector3 diff = new Vector3(Speed * Time.deltaTime, 0, 0) -
-                                           new Vector3(0, -Speed * Time.deltaTime, 0);
-                            transform.Translate(diff);
-                        }
-                        //rotate to look at the player
-                        transform.LookAt(_targetPlayer.position);
-                        //correcting the original rotation
-                        transform.Rotate(new Vector3(0, -90, transform.rotation.z - 90), Space.Self);
+                    if (Vector3.Distance(transform.position, _targetPlayer.position) > MinDist){
+                        //move if distance from target is greater than 1
+                        Vector3 diff = new Vector3(Speed * Time.deltaTime, 0, 0) -
+                                       new Vector3(0, -Speed * Time.deltaTime, 0);
+                        transform.Translate(diff);
                     }
+                    //rotate to look at the player
+                    transform.LookAt(_targetPlayer.position);
+                    //correcting the original rotation
+                    transform.Rotate(new Vector3(0, -90, transform.rotation.z - 90), Space.Self);
                 }
             }
         }
 
         private void Shoot(){
-            if (Time.time > _currentTime && _targetPlayer != null && PlayerDetected &&
-                Vector3.Distance(transform.position, _targetPlayer.position) <= MaxDist){
-                Debug.Log("Prepared to fire! Canons: " + _canons);
+            if (Time.time > _currentTime && _targetPlayer != null && PlayerDetected){
                 foreach (Canon canon in _canons){
-                    Debug.Log("Fire!");
                     GameManager.Instantiate("Actors/Ammo", canon.transform, transform.parent.gameObject);
                     _currentTime = Time.time + TimeShooting;
                 }
             }
+        }
+
+        void OnTriggerEnter(Collider other){
+            Debug.Log("Enter: " + other);
+            PlayerDetected = true;
+        }
+
+        void OnTriggerStay(Collider other){
+            Shoot();
+        }
+
+        void OnTriggerExit(Collider other){
+            Debug.Log("Exit: " + other);
+            PlayerDetected = false;
         }
     }
 }
